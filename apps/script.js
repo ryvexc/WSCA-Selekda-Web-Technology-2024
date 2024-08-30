f439ff0c0937ca7968c792757ebfef90151d7672
Arif Kurniawan <ryvexc@gmail.com>
2024-08-30 12:54:41 +0700

    creating dsgn

---
+++ apps/script.js
@@ -0,0 +1,39 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let painting = false;
+
+const brushSize = 1;
+
+function startPosition(e) {
+	painting = true;
+	draw(e);
+}
+
+function endPosition() {
+	painting = false;
+	ctx.beginPath();
+}
+
+function draw(e) {
+	if (!painting) return;
+
+	ctx.lineWidth = brushSize;
+	ctx.lineCap = "round";
+	ctx.strokeStyle = "rgb(0,0,0)";
+
+	const canvasClientRect = canvas.getBoundingClientRect();
+
+	ctx.lineTo(e.clientX - canvasClientRect.x, e.clientY - canvasClientRect.y);
+	ctx.stroke();
+	ctx.beginPath();
+	ctx.moveTo(e.clientX - canvasClientRect.x, e.clientY - canvasClientRect.y);
+}
+
+canvas.addEventListener("mousedown", startPosition);
+canvas.addEventListener("mouseup", endPosition);
+canvas.addEventListener("mousemove", draw);
+
+function clearCanvas() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
